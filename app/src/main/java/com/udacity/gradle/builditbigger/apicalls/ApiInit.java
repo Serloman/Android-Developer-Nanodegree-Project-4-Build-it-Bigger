69aa9667316a79d060ac4baafc6ecf44dc20e257
Sergio López Márquez <serloman@gmail.com>
2015-08-15 01:15:39 +0200

    Minor update

--- app/src/main/java/com/udacity/gradle/builditbigger/apicalls/ApiInit.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/apicalls/ApiInit.java
@@ -14,15 +14,16 @@ import java.io.IOException;
  * Based on https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
  */
 public class ApiInit {
+    // options for running against local devappserver
+    // - 10.0.2.2 is localhost's IP address in Android emulator
+    // - turn off compression when running against local devappserver
+    private final static String mRootUrl = "http://10.0.2.2:8080/_ah/api/";
     private static MyApi myApiService;
 
     public static MyApi getInstance() {
         if(myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl(mRootUrl)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
